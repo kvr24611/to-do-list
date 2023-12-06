7d3092989e0e8b99e7b8532726739286a4ca855f
KaVeer <kvr24611@gmail.com>
2023-12-06 15:56:43 +0545

    Implement remove and list methods

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# To-Do List with File Storage:
-# A to-do list application that allows users to add, remove, and view tasks.
-# And save tasks to a text file so that they persist between program runs.
-
+'''To-Do List with File Storage:
+   * A to-do list application that allows users to add, remove, and view tasks.
+   * And save tasks to a text file so that they persist between program runs.
+'''
 import json
+import pprint
 
 class TaskManager:
     '''Task Class'''
@@ -31,15 +32,43 @@ class TaskManager:
         self.tasks.append(new_task)
         self.write_tasks_to_file()
 
+    def remove_task(self, task_identifier):
+        """
+        Remove a task from the task list.
 
+        Parameters:
+        - task_identifier : task_name
+        """
+        self.tasks = [d for d in self.tasks if d['task'] != task_identifier]
+        self.write_tasks_to_file()
+
+    def list_tasks(self):
+        """
+        List all tasks in the To-Do List.
+        """
+        # Display all tasks along with their details (task name, due date, status)
+        if self.tasks:
+            print("Your To-Do List:")
+            for i, tsk in enumerate(self.tasks, 1):
+                print(f"{i}. Task: {tsk['task']}, Due Date: {tsk['due_date']}, Status: {tsk['status']}")
+        else:
+            print("Your To-Do List is empty.")
+        
 
 # Instatiate a TaskManager object
 my_task_manager = TaskManager("tasks.json")
 
-
+# Add Task
 task_name = input("Task name: ")
 status = input("Status: ")
 due_date = input("Due date: ")
 task = json.loads(f'{{"task": "{task_name}", "status": "{status}", "due_date": "{due_date}"}}')
-
 my_task_manager.add_task(task)
+
+# View Tasks
+pprint.pprint(my_task_manager.read_tasks_from_file("tasks.json"))
+my_task_manager.list_tasks()
+
+# Remove Task
+task_name_to_del = input('Enter the task name you wanna delete: ')
+my_task_manager.remove_task(task_name_to_del)
